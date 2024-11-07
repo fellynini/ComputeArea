c6fd5fb99f512dbb4421290823b3cd29980ebd43
fellynini <fellynini4@gmail.com>
2024-11-07 23:48:20 +0300

    first commit

---
+++ ComputeArea.java
@@ -0,0 +1,19 @@
+public class ComputeArea {
+
+public static void main( String[] args ){
+
+double radius;
+double area;
+//assign a radius 
+radius = 20;
+
+//assign the area
+
+area = radius * radius * 3.1495 ;
+
+//display the area
+
+System.out.println( "The area of the circle of radius " + radius + " is " +area );
+
+}
+}
